8b0235ab45a2a1114cf055fd388c19b7f54b59ec
Joel Alexander <a.joeljr@hotmail.com>
2024-02-23 11:42:56 -0500

    initial commit

---
+++ agents/tools/countries_image_generator.py
@@ -0,0 +1,14 @@
+from langchain.tools import tool
+from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
+
+
+@tool
+def countries_image_generator(country: str):
+    """Call this to get an image of a country"""
+    res = DallEAPIWrapper(model="dall-e-3").run(f"You generate image of a country representing the most "
+                                                f"typical country's characteristics, incorporating its flag. "
+                                                f"the country is {country}")
+
+    answer_to_agent = (f"Use this format- Here is an image of {country}: [{country} Image]"
+                       f"url= {res}")
+    return answer_to_agent
